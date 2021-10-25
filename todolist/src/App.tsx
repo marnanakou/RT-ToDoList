41f34dada9aac036cf227de7cfbab7989f0059ff
Maria Nanakou <marnanakou@gmail.com>
2021-10-25 19:09:57 +0200

    defining Typescript types

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,13 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC,useState,ChangeEvent} from 'react';
 import "./App.css"
 
 const App : FC = () =>{
+  const [task, setTask]=useState<string>("")
+  const [deadline, setDeadline]=useState<number>(0)
+  const [todo, setTodoList]=useState([])
+
+  const handleChange = (e:ChangeEvent<HTMLInputElement>) =>{
+
+  }
+
   return (
     <div className="App">
       <div className="header">
         <div className="inputContainer">
-        <input type= "text" placeholder="Task... "></input>
-        <input type= "number" placeholder="Deadline in Days... "></input>
+        <input type= "text" placeholder="Task... " onChange={handleChange}></input>
+        <input type= "number" placeholder="Deadline in Days... " onChange={handleChange}></input>
         </div>
         <button >Add Task</button>
       </div>
