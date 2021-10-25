6c28c7c0f215a3123526bedd97429cf19257f450
Maria Nanakou <marnanakou@gmail.com>
2021-10-25 16:25:31 +0200

    removing some not useful logos and files

---
+++ todolist/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="ToDoList">
+      </header>
+    </div>
+  );
+}
+
+export default App;
